3ee09836d91375187b712b80c922d423dc0be6fe
hellosidney9699 <iraskalozub9914@gmail.com>
2023-05-31 23:34:15 +0300

    dashboard

--- src/route/index.js
+++ src/route/index.js
@@ -1121,11 +1121,11 @@ router.get('/slack', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/slack-2', function (req, res) {
+router.get('/dashboard', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('slack-2', {
+  res.render('dashboard', {
     layout: null,
   })
   //                  ↑↑ сюди вводимо JSON дані
